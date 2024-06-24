1f039b9cbed432244d94734858f7f380ec91f2d5
Shuga8 <lotocharles8@gmail.com>
2024-06-24 20:55:32 +0100

    deploying contracts, apiFeatures, && setting structures

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {},
+
+  },
+  ignition: {
+    modules: [
+      require("./ignition/modules/Lock.js"),
+    ],
+  },
 };
